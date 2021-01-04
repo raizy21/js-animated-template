c98314c64b9d95284f64145b30268c07f850e87d
raizy21 <chiper_raizy@yahoo.com>
2021-01-04 12:32:10 +0100

    animated-template_code

---
+++ script.js
@@ -0,0 +1,20 @@
+/*********************
+ * Links
+ *
+ * {@link} - https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+ * {@link} - https://webformatter.com/html
+ * {@link} - https://fontawesome.com/icons?d=gallery&q=close&m=free
+ * {@link} - https://unsplash.com/
+ * {@link} - https://randomuser.me/
+ * {@link} - https://michalsnik.github.io/aos/
+ * {@link} - https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/loading-third-party-javascript
+ * {@link} - https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/loading-third-party-javascript#use_async_or_defer
+ **********************/
+
+//  aos init
+AOS.init({
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+});
